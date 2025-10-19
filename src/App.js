80f426830b194984c9318f67cc8be03a4d67d01e
Kamikadze57 <savickijpavel42@gmail.com>
2025-10-19 10:17:55 +0300

    add files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
+import TaskList from "./Components/TaskList";
+
+const initialTasks = [
+  { id: 1, text: "Купити ковбасу", completed: false },
+  { id: 2, text: "Зробити домашку", completed: true },
+  { id: 3, text: "Влаштуватись на роботу", completed: false },
+];
 
 function App() {
+  const [tasks, setTasks] = useState(initialTasks);
+  const DeleteTask = (taskId) => {
+    const updatedTasks = tasks.filter((task) => task.id !== taskId);
+    setTasks(updatedTasks);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <h1 className="title">Плани</h1>
+      <div className="box">
+      <TaskList tasks={tasks} onDelete={DeleteTask} />
+      </div>
     </div>
   );
 }
---
+++ src/Components/TaskList.jsx
@@ -0,0 +1,28 @@
+import { Component } from "react";
+
+class TaskList extends Component {
+  DeleteTask = (taskId) => {
+    this.props.onDelete(taskId);
+  };
+  render() {
+    const { tasks } = this.props;
+    if (!tasks || tasks.length === 0) {
+      return <p className="no-tasks__text">Немає завдань у списку</p>;
+    }
+    return (
+      <ul className="task__List">
+        {tasks.map((task) => (
+          <li className="task__item" key={task.id}>
+            <p>
+              {task.text}
+              {task.completed && " (Виконано)"}
+            </p>
+            <button onClick={() => this.DeleteTask(task.id)}>Видалити</button>
+          </li>
+        ))}
+      </ul>
+    );
+  }
+}
+
+export default TaskList;
